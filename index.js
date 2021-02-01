e46939cfe2e607bba159fe752e35e17c4ffc36f7
sam <samxtasnim@gmail.comgit confing --global user.emailgit config --global user.email samxtasnim@gmail.com>
2021-02-01 15:27:56 +0600

    project completed

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 function getId() {
   let randomPin = Math.ceil(Math.random() * 10000);
   if(randomPin.toString().length < 4) {
-    randomPin = Math.ceil(Math.random() * 10000);
-    document.getElementById("generated-pin").value = randomPin;
+   return getId();
   }
   document.getElementById("generated-pin").value =  randomPin;
 }
@@ -14,21 +13,29 @@ function showInput(id) {
 function clearInput() {
   document.getElementById("button-output").value = ""
 }
+
 function matchPin() {
   if(document.getElementById("generated-pin").value === "") {
     alert("Please generate a pin to continue")
     return ;
   }
-  if( parseInt(document.getElementById("generated-pin").value) === parseInt(document.getElementById("button-output").value) ) {
+  else if(parseInt(document.getElementById("try-count").innerText) <= 0 ) {
+    alert("Sorry your cannot try again");
+    return;
+  }
+  else if( parseInt(document.getElementById("generated-pin").value) === parseInt(document.getElementById("button-output").value) ) {
     document.getElementById("notify-positive").style.display = "block";
+    document.getElementById("count-paragraph").style.display = "none"
     clearInput()
-  } else {
+  } 
+  else {
     document.getElementById("notify-negative").style.display = "block";
     document.getElementById("notify-positive").style.display = "none";
+    document.getElementById("try-count").innerText = parseInt(document.getElementById("try-count").innerText) - 1;
     clearInput()
   }
 }
 
 function clearPrevious() {
-  document.getElementById("button-output")
+  document.getElementById("button-output").value = document.getElementById("button-output").value.slice(0, -1);
 }
